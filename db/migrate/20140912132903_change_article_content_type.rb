cff8b67a6341c1cd4ceec20cb2acd78321835d01
Arun Kaushik <arunkaushik@freshdesk.com>
2014-09-12 19:10:28 +0530

    changing article content type to text for larger input

---
+++ db/migrate/20140912132903_change_article_content_type.rb
@@ -0,0 +1,10 @@
+class ChangeArticleContentType < ActiveRecord::Migration
+
+  	def up
+    	change_column :articles, :content, :text
+	end
+
+	def down
+    	change_column :articles, :content, :string
+	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140911070631) do
+ActiveRecord::Schema.define(:version => 20140912132903) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
-    t.string   "content"
+    t.text     "content",    :limit => 255
     t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
     t.string   "tag"
   end
 
